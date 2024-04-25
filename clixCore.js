3099e43390a98ff59dcfd43012a5d6103343e53b
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-04-25 22:05:06 +0300

    update

--- clixCore.js
+++ clixCore.js
@@ -1,4 +1,7 @@
 var wallet = 0;
+var second = new Date().getSeconds();
+
+var autoClickArray = [];
 
 var prodValues = {
     totalProducedEnergy: 0,
@@ -14,7 +17,19 @@ var levelValues = {
     speedBonus: .25,
     status: false
 };
+
+var bonuses = {
+    speedBonus: 100,
+    totalEnergyBonus: 100,
+    autoClickBonus: 100
+};
 // Game Data
+
+const updateBonuses = () => {
+    $("#speedBonusPrice").text(bonuses.speedBonus.toFixed(2));
+    $("#totalEnergyBonusPrice").text(bonuses.totalEnergyBonus.toFixed(2));
+    $("#autoClickBonusPrice").text(bonuses.autoClickBonus.toFixed(2));
+};
 
 const updateWallet = () => {
     $("#wallet").text(wallet.toFixed(2));
@@ -34,6 +49,12 @@ const updateProdStatus = () => {
     $("#jPerWatt").text(prodValues.jPerWatt);
     $("#gain").text(prodValues.gain);
 };
+
+const click = () => {
+    prodValues.totalClick += 1;
+    totalProducedEnergy += (1 - (Math.E / Math.PI)) * speedBonus;
+    //(Math.random() / Math.E)**Math.PI;//(0.09 + (Math.random() / (Math.E)));
+}
 // Functions
 
 var elapsedTime = prodValues.elapsedTime;
@@ -43,17 +64,13 @@ var gain = prodValues.gain;
 var energyToBeProduced = levelValues.energyToBeProduced;
 var speedBonus = levelValues.speedBonus;
 
-$("#produce").on("click", () => {
-    prodValues.totalClick += 1;
-    totalProducedEnergy += (1 - (Math.E / Math.PI)) * speedBonus;
-    //(Math.random() / Math.E)**Math.PI;//(0.09 + (Math.random() / (Math.E)));
-});
+$("#produce").on("click", click);
 
 $("#levelUpButton").on("click", () => {
     if (totalProducedEnergy - levelValues.energyToBeProduced > 0) {
         totalProducedEnergy -= levelValues.energyToBeProduced;
         levelValues.level += 1;
-        energyToBeProduced += (Math.random() * 10 + 10);
+        energyToBeProduced += (Math.random() * speedBonus + 10) + (energyToBeProduced);
         speedBonus *= 1.5;
 
         gain += (jPerWatt / totalProducedEnergy);
@@ -62,10 +79,34 @@ $("#levelUpButton").on("click", () => {
     }
 });
 
+$("#speedBonusx").on("click", () => {
+    if (wallet >= bonuses.speedBonus) {
+        speedBonus += 1/levelValues.energyToBeProduced + speedBonus;
+        wallet -= bonuses.speedBonus;
+        bonuses.speedBonus *= (bonuses.speedBonus/51);
+    }
+});
+
+$("#totalEnergyBonus").on("click", () => {
+    if (wallet >= bonuses.totalEnergyBonus) {
+        totalProducedEnergy += totalProducedEnergy * 0.15;
+        wallet -= bonuses.totalEnergyBonus;
+        bonuses.totalEnergyBonus += (bonuses.totalEnergyBonus/21);
+    }
+});
+
+$("#autoClickBonus").on("click", () => {
+    if (wallet >= bonuses.autoClickBonus) {
+        autoClickArray.push(click);
+        wallet -= bonuses.autoClickBonus;
+        bonuses.autoClickBonus *= (bonuses.autoClickBonus/40);
+    }
+});
+
 const main = () => {
     setInterval(() => {
         elapsedTime = elapsedTime + 1 / 10;
-        jPerWatt = (totalProducedEnergy / (elapsedTime/prodValues.totalClick)) * levelValues.speedBonus;
+        jPerWatt = (totalProducedEnergy / ((elapsedTime)/prodValues.totalClick)) * levelValues.speedBonus;
         // Calculations
 
         if (elapsedTime.toFixed(2) % 60 == 0) {
@@ -74,6 +115,20 @@ const main = () => {
         }
         gain = 0;
         // Withdraw
+        
+        /*var sec = new Date().getSeconds();
+        if (second < sec) {            
+            // elapsedTime += 1;
+
+            autoClickArray.forEach(clickFunction => {
+                clickFunction();
+            });
+            second = sec;
+        }*/
+
+        autoClickArray.forEach(clickFunction => {
+            clickFunction();
+        });
 
         prodValues.elapsedTime = Math.floor(elapsedTime);
         prodValues.jPerWatt = jPerWatt.toFixed(2);
@@ -85,8 +140,15 @@ const main = () => {
 
         updateLevelStatus();
         updateProdStatus();
+        updateBonuses();
         updateWallet();
     }, 100);
 }
+
+$(document).on("keydown", (event) => {
+    if (event.key === "q") {
+        wallet += 100;
+    }
+});
 
 main();
